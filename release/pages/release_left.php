18816daf1ed07cbbdb5bd6f31cd880b3f2d65119
dragonkwaaa <hachlingdragon@hanmail.net>
2021-01-27 15:16:07 +0900

    1. 진맛과 웹 작업. [20210127_1240 완료] -> 2. 출고관리 페이지 작업. [20210127_1240 완료] -> 3. release\index.php 작업. [20210127_1209 완료] -> 3. 단일 테이블 작업. [20210127_1209 완료] -> 3. 상세 정보는 팝업 표시. [20210127_1209 완료] -> 3. 팝업 내, 레지스트 구역과 테이블 구역의 두가지가 들어가야 함. [20210127_1209 완료] -> 3. 외부 테이블은 고정 텍스트로. [20210127_1209 완료] -> 3. 외부 테이블 등록은 팝업으로. [20210127_1206 완료] -> 2. 구성요소의 css를 수정하기. [20210127_1206 완료] -> 3. 우측 테이블의 검색 버튼 크기 줄이기. [20210127_1206 완료] -> 4. 위치도 우측 테이블 내부로 넣어서 숨겨지거나 표시되게 하기. [20210127_1206 완료] -> 4. common\css\style.css 작업. [20210127_1206 완료] -> 4. order\indㅡex.php 작업. [20210127_1509 완료] -> 2. 출고예약관리 페이지 작업. [20210127_1509 완료] -> 3. release\reservation.php 작업. [20210127_1410 완료] -> 3. 풀 테이블 사용. [20210127_1433 완료] -> 3. 상세 버튼으로 팝업 진입. [20210127_1509 완료] -> 3. 팝업 테이블 수정. [20210127_1509 완료] -> 4. 상단에 인풋창 그룹 추가. [20210127_1509 완료] -> 4. 하단에 품목 목록 테이블 추가.

--- release/pages/release_left.php
+++ release/pages/release_left.php
@@ -4,7 +4,13 @@
 			<li>
 				<a href="/release/" class="<?=($lCode == '600' ? ' activated' : '')?>">
 					<i class="dashboardIcon"></i>
-					<span class="ml11">발주요청 관리</span>
+					<span class="ml11">출고(주문) 관리</span>
+				</a>
+			</li>
+			<li>
+				<a href="/release/reservation" class="<?=($lCode == '601' ? ' activated' : '')?>">
+					<i class="dashboardIcon"></i>
+					<span class="ml11">출고 예약 관리</span>
 				</a>
 			</li>
 		</ul>
