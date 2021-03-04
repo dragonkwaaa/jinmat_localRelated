63a3755d3b819cf69a8222fe10accb684b90143a
dragonkwaaa <hachlingdragon@hanmail.net>
2021-03-04 20:20:57 +0900

    [20210304_2019 완료] -> 1. 진맛과 wms 퍼블리싱 수정하기. -> 동영씨와의 상의 내용 작업하기. [20210304_2019 완료] -> 2. 대시보드 페이지에 내용 추가하기. [20210304_2019 완료] -> 3. 창고별 재고량, 입고량, 출고량을 표시하기. [20210304_2019 완료] -> 4. 좌측에 막대그래프(재고/입고/출고 3막대)로 만들기. [20210304_2019 완료] -> 4. 우측에 테이블로 만들기. [20210304_2019 완료] -> 5. "index.php" 작업.

--- index.php
+++ index.php
@@ -42,6 +42,69 @@
 					</div>
 				</div>
 			</div>
+			<!-- :: starting : ksg_20210304_1813 : [진맛] 창고현황 파트. -->
+			<div class="section dashTableSort mt34">
+				<div class="titleBox sectionSort">
+					<div>최근 창고현황</div>
+					<a href="javascript:Void(0);" class="btn regLinkBtn">더 보기</a>
+				</div>
+				<div class="conGroup inHorizon_double">
+					<!-- :: starting : ksg_20210304_1819 : [땡땡] 구역에서 절반의 너비를 차지하는 그래프 내용. -->
+					<div class="graphCase lSide_relaNoScr">
+						<canvas id="barChart_1" style="height : 400px; width : 100%; min-width : 246px;"></canvas>
+					</div>
+					<!-- :: starting : ksg_20210304_1818 : [땡땡] 구역에서 절반의 너비를 차지하는 테이블 내용. 좌측에 그래프가 있을 때의 모습. -->
+					<table class="listTable rSide_absol">
+						<colgroup>
+							<col style="width : 500px">
+							<col style="width : 500px">
+							<col style="width : 500px">
+							<col style="width : 500px">
+							<col style="width : 500px">
+						</colgroup>
+						<thead>
+						<tr>
+							<th>창고명</th>
+							<th>재고량</th>
+							<th>입고량</th>
+							<th>출고량</th>
+						</tr>
+						</thead>
+						<tbody>
+						<tr>
+							<td>서울창고</td>
+							<td>4,200</td>
+							<td>8,000</td>
+							<td>1,200</td>
+						</tr>
+						<tr>
+							<td>경기창고</td>
+							<td>4,150</td>
+							<td>7,500</td>
+							<td>500</td>
+						</tr>
+						<tr>
+							<td>대전창고</td>
+							<td>3,950</td>
+							<td>4,200</td>
+							<td>700</td>
+						</tr>
+						<tr>
+							<td>옥천창고</td>
+							<td>3,900</td>
+							<td>100</td>
+							<td>3,500</td>
+						</tr>
+						<tr>
+							<td>부산창고</td>
+							<td>3,950</td>
+							<td>2,000</td>
+							<td>7,000</td>
+						</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
 			<div class="section dashTableSort mt34">
 				<div class="titleBox sectionSort">
 					<div>발주승인요청내역</div>
@@ -128,7 +191,188 @@
 		</div>
 	</div>
 </div>
+<script src="/common/js/Chart.min.js"></script>
 <script>
+// :: open : ksg_20210304_1838 : [세무야] 바 형태의 그래프. Y축 최하단에서부터 시작하는 내용. 한 년도당 3개씩 표시.
+$( document ).ready( function(){
+    let chart    = document.getElementById('barChart_1').getContext('2d'),
+    // :: 그래프 내부의 그라디언트 색상 관련.
+    gradient1 = chart.createLinearGradient(0, 0, 0, 500);
+    gradient1.addColorStop(0.5, '#a2bcef');
+    gradient1.addColorStop(0.7, '#f8fafe');
+    gradient1.addColorStop(1, '#fff');
+
+    gradient2 = chart.createLinearGradient(0, 0, 0, 280);
+    gradient2.addColorStop(0, 'cornFlowerBlue');
+    gradient2.addColorStop(0.4, 'lightBlue');
+    gradient2.addColorStop(1, '#fff');
+
+    // :: 측정값 데이터 들어가는 자리.
+    let data  = {
+        labels: [ '2009', '', '2010', '', '2011', '', '2012', '', '2013', '', '2014', '', '2015', '', '2016', '', '2017', '', '2018', '', '2019', '', '2020', '', '2021' ],
+        datasets: [{
+    		label: 'Custom Label Name',
+    		pointBackgroundColor: '#4d42e5',
+    		pointRadius: 1,
+    		pointHoverRadius: 1,
+    		borderWidth: 1,
+    		borderColor: '#5a51e7',
+    		data: [0, 0, 0, 0, 0, 0, 10, 40, 40, 40, 40, 70, 70, 65, 65, 150, 150, 27, 27, 27, 15, 0, 0, 0, 0]
+        }]
+    };
+    // :: 차트 옵션
+    let options = {
+    	layout: {
+            padding: {
+                left: 0,
+                right: 0,
+                top: 30,
+                bottom: 0
+    		},
+    		margin: {
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+    		}
+        },
+    	responsive: false,
+    	maintainAspectRatio: true,
+    	animation: {
+    		easing: 'easeInOutQuad',
+    		duration: 520
+        
+    	},
+    	scales: {
+    		xAxes: [{
+    			gridLines: {
+    				color: 'rgba(200, 200, 200, 0.05)',
+    				lineWidth: 1
+    			},
+    			ticks: {
+                    autoSkip: false,
+                    maxRotation: 0,
+    				// minRotation: 0
+    				fontSize: 10,
+    				fontStyle : 600
+                }
+    		}],
+    		yAxes: [{
+    			gridLines: {
+    				color: '#a2bcef',
+    				lineWidth: 1,
+    				drawBorder: false
+    			},
+    			ticks: {
+                
+    				autoSkip: false,		// :: y축 단위계 생략되지 않게 하는 내용 1.
+    				stepSize : 1,			// :: y축 단위계 생략되지 않게 하는 내용 2.
+    				min: 0,
+    				max: 10000,
+    				maxRotation: 0,
+    				padding : 10,
+    				fontSize: 10,
+    				fontStyle : 600,
+    				fontColor : '#000',
+    				fontFamily : 'Noto Sans KR',
+    				// :: 특정 y축 단위계의 명칭을 커스터마이징.
+    				callback: function(label, index, labels) {
+						// :: open : ksg_20210304_1900 : [진맛] 띄어쓰기가 들어간 텍스트를 입력할때의 구조. 주석 처리 하여 보존함.
+    					// let	worstText		=	"매우<br>나쁨";
+    					// let	spacedWorstText	=	worstText.split("<br>");
+						// :: close : ksg_20210304_1900 : [진맛] 띄어쓰기가 들어간 텍스트를 입력할때의 구조. 주석 처리 하여 보존함.
+    					switch (label) {
+    					  	case 2000:
+    							return '2000';
+    					  	case 4000:
+    							return '4000';
+    					  	case 6000:
+    							return '6000';
+							case 8000:
+    							return '8000';
+							case 10000:
+    							return '10000';
+						  	// :: open : ksg_20210304_1900 : [진맛] 띄어쓰기가 들어간 텍스트를 입력할때의 구조. 주석 처리 하여 보존함.
+    					  	// case 150:
+    						// 	return spacedWorstText;
+							// :: close : ksg_20210304_1900 : [진맛] 띄어쓰기가 들어간 텍스트를 입력할때의 구조. 주석 처리 하여 보존함.	
+    					}
+    				}
+    			}
+    		}]
+    	},
+    	elements: {
+    		line: {
+                // :: open : ksg_20210201_1907 : [공기] 그래프의 선을 곡선으로 만드는 내용.(높은 숫자일수록 곡선이 된다.)
+                tension: 0.3
+                // :: close : ksg_20210201_1907 : [공기] 그래프의 선을 곡선으로 만드는 내용.(높은 숫자일수록 곡선이 된다.)
+    		}
+    	},
+    	legend: {
+    		display: true,
+			position : 'left'
+    	},
+    	point: {
+    		backgroundColor: 'white'
+    	},
+    	tooltips: {
+    		titleFontFamily: 'Open Sans',
+    		backgroundColor: 'rgba(0,0,0,0.3)',
+    		titleFontColor: 'red',
+    		caretSize: 5,
+    		cornerRadius: 2,
+    		xPadding: 10,
+    		yPadding: 10
+    	}
+    };
+    let dataFirst = {
+		label : "재고량",
+        data: [4200, 4150, 3950, 3900, 3950],
+        lineTension: 0,
+        fill: true,
+        backgroundColor: gradient1,
+        borderColor: '#ccc',
+        borderWidth : 1,
+        pointRadius: 1,
+    	pointHoverRadius: 1,
+    	lineTension: 0.5           
+    };
+    let dataSecond = {
+		label : "입고량",
+        data: [8000, 7500, 4200, 100, 2000],
+        lineTension: 0,
+        fill: true,
+        backgroundColor: "navy",
+        borderColor: 'navy',
+        borderWidth : 1,
+        pointRadius: 1,
+    	pointHoverRadius: 1,
+    	lineTension: 0.5
+    };
+	let dataThird = {
+		label : "출고량",
+        data: [1200, 500, 700, 3500, 7000],
+        lineTension: 0,
+        fill: true,
+        backgroundColor: "#cecece",
+        borderColor: '#ccc',
+        borderWidth : 1,
+        pointRadius: 1,
+    	pointHoverRadius: 1,
+    	lineTension: 0.5
+    };
+    var speedData = {
+        labels: [ '서울창고', '경기창고', '대전창고', '옥천창고', '부산창고' ],
+      datasets: [dataFirst, dataSecond, dataThird]
+    };
+    let chartInstance = new Chart(chart, {
+        type: 'bar',
+        // data: data,
+        data : speedData,
+    	options: options
+    });
+});
+// :: close : ksg_20210304_1838 : [세무야] 바 형태의 그래프. Y축 최하단에서부터 시작하는 내용. 한 년도당 3개씩 표시.
 </script>
 </body>
 </html>
